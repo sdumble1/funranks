cc5fe072708dce59791f1c194834493a8719a625
sdumble1 <s.dumble@stats4sd.org>
2018-12-11 11:07:07 +0000

    Add files via upload

--- R/boot_rank.R
+++ R/boot_rank.R
@@ -14,6 +14,7 @@ boot_rank<-function(data,IDcol,formula,nboot=100,diagnostics=TRUE,CIplot=TRUE,le
   require(dplyr)
   require(ggplot2)
   require(brglm)
+  require(purrr)
 
   bootres<-NULL
   if(length(IDcol)==nrow(data)){
@@ -29,34 +30,48 @@ boot_rank<-function(data,IDcol,formula,nboot=100,diagnostics=TRUE,CIplot=TRUE,le
   sel<- sample %>% map_df(~filter(data,ID==.))
 
   ####temporary hack - if not all selections present then cheat and restart
-  if(any(colSums(abs(model.frame(f,data=modeldata2)))==0)){
+  frame<-model.frame(formula,data=modeldata2)
+  numberfy<-function(x){
+    if(class(x)=="character"|class(x)=="factor"){
+      x<-as.numeric(as.factor(x))-1
+    }
+    return(x)
+  }
+  frame<-sapply(frame,numberfy)
+  
+  
+  if(any(colSums(frame)==0)){
     i<-i-1
   }
   else{
-    m0<-suppressWarnings(brglm(data=sel, f, family=binomial))
+    m0<-suppressWarnings(brglm(data=sel, formula, family=binomial))
     fulldata<-data.frame(coef=coefficients(m0),option=names(coefficients(m0)),boot=i)
-    fulldata$coef[is.na(fulldata$coef)]<-0
+    
+    #not all NA are created equal - if it is the final option then NA is fine otherwise NA is NA
+    fulldata$coef[grep(colnames(frame)[ncol(frame)],names(coefficients(m0)))]<-0
     bootres<-rbind(bootres,fulldata)
     setTxtProgressBar(pb, i)
     }
 
   }
-  bootres$option<-reorder(bootres$option,bootres$coef,mean)
-  estimates<-bootres %>% group_by(option) %>% summarise(mean=mean(coef),lowerCI=quantile(coef,(1-level)/2),upperCI=quantile(coef,1-((1-level)/2)))
+  
+  bootres$option<-reorder(bootres$option,bootres$coef,mean,na.rm=TRUE)
+  estimates<-bootres %>% group_by(option) %>% dplyr::summarise(est=median(coef,na.rm=T),lowerCI=quantile(coef,(1-level)/2,na.rm=T),
+                                                               upperCI=quantile(coef,1-((1-level)/2),na.rm=T))
   output<-list(estimates=estimates,boots=bootres)
   if(diagnostics==TRUE){
 
     p1<- ggplot(data=bootres,aes(y=coef,x=option))+
       geom_violin()+
       geom_jitter(alpha=0.2,width = 0.2,height=0)+
-      stat_summary(geom="point",col="red",fun.y = "mean",size=4,shape=4)
+      stat_summary(geom="point",col="red",fun.y = "median",size=4,shape=4)
 
     plot(p1)
     output$diagnostics<-p1
   }
   if(CIplot==TRUE){
 
-    p2<- ggplot(data=estimates,aes(y=mean,x=option,ymax=upperCI,ymin=lowerCI))+
+    p2<- ggplot(data=estimates,aes(y=est,x=option,ymax=upperCI,ymin=lowerCI))+
       geom_point()+
       geom_errorbar()
 
